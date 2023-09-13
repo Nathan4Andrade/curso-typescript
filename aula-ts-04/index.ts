92e45d1dce95be5c34778dd6d7e332b63f66caf1
Nathan4Andrade <nathanandrade20@gmail.com>
2023-09-13 19:38:13 -0300

    aula 04

--- aula-ts-04/index.ts
+++ aula-ts-04/index.ts
@@ -1,37 +1,32 @@
-function multiply(num1: number | string | null, num2: number | string | null) {
-  if (typeof num1 === null || typeof num2 === null)
-    console.log("Não é possível executar com esta entrada");
-
-  return Number(num1) * Number(num2);
+function multiply(num1: number, num2: number) {
+  return num1 * num2;
 }
 
-function sum(num1: number | string | null, num2: number | string | null) {
-  if (typeof num1 === null || typeof num2 === null)
-    console.log("Não é possível executar com esta entrada");
-
-  return Number(num1) + Number(num2);
+function sum(num1: number, num2: number) {
+  return num1 * num2;
 }
 
-function isEven(num: number | string | null) {
-  if (typeof num === null)
-    console.log("Não é possível executar com esta entrada");
-
-  return Number(num) % 2 === 0;
+function isEven(num: number) {
+  return num % 2 === 0;
 }
 
 function showResult(result: number) {
   if (isEven(result)) {
     console.log(`The result is ${result} and it's even!`);
   } else {
-    console.log(`The result is ${result} and it's odd!`);
+    console.log(`The result is ${result} and it's even!`);
   }
 }
 
 (() => {
-  const num1 = prompt("First Number");
-  const num2 = prompt("Second Number");
+  const num1 = Number(prompt("First Number"));
+  const num2 = Number(prompt("Second Number"));
 
-  let result = sum(num1, num2);
-  result += multiply(1, 2);
-  showResult(result);
+  if (!isNaN(num1) && !isNaN(num2)) {
+    let result = sum(num1, num2);
+    result += multiply(1, 2);
+    showResult(result);
+  } else {
+    console.log("Não é possível executar com estes valores.");
+  }
 })();
